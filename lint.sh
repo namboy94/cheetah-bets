a295eab7fffc29524c0c44b073318b2a2c021b30
Hermann Krumrey <hermann@krumreyh.com>
2017-07-13 13:19:41 +0200

    Fix conditionals in bash scripts

--- lint.sh
+++ lint.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 if [ -z "$SHOW_LINT_RESULTS" ]; then
-    php vendor/phpcheckstyle/phpcheckstyle/run.php --src src --config checkstyle.xml || firefox style-report/index.html
-    php vendor/phpcheckstyle/phpcheckstyle/run.php --src test --config checkstyle.xml || firefox style-report/index.html
-else
     php vendor/phpcheckstyle/phpcheckstyle/run.php --src src --config checkstyle.xml
     php vendor/phpcheckstyle/phpcheckstyle/run.php --src test --config checkstyle.xml
+else
+
+    php vendor/phpcheckstyle/phpcheckstyle/run.php --src src --config checkstyle.xml || firefox style-report/index.html
+    php vendor/phpcheckstyle/phpcheckstyle/run.php --src test --config checkstyle.xml || firefox style-report/index.html
 fi
--- unittest.sh
+++ unittest.sh
@@ -8,5 +8,7 @@ fi
 vendor/bin/phpunit test --coverage-html=coverage
 
 if [ -z "$SHOW_COVERAGE" ]; then
+    echo "Not Displaying Coverage"
+else
     firefox coverage/index.html
 fi
